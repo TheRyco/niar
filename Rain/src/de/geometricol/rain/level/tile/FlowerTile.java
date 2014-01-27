d4ffc4eaa0d8a1cd21a5a7aca4cb4f371d3409d5
Rico Christiansen <sericose@aol.com>
2014-01-27 20:06:06 +0100

    zweiter test

---
+++ Rain/src/de/geometricol/rain/level/tile/FlowerTile.java
@@ -0,0 +1,18 @@
+package de.geometricol.rain.level.tile;
+
+import de.geometricol.rain.graphics.Screen;
+import de.geometricol.rain.graphics.Sprite;
+
+public class FlowerTile extends Tile{
+
+	public FlowerTile(Sprite sprite) {
+		super(sprite);
+	}
+	
+	public void render(int x, int y, Screen screen) {
+		screen.renderTile(x << 4, y << 4, this);
+	}
+	
+	
+
+}
